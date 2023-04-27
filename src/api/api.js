dead1c73532bd6d5805b70859cd5efb7a5ef10a6
pdev6373 <pdev6373@gmail.com>
2023-04-27 02:55:49 +0100

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// ================= Products Data Fetching Start here ===============
+export async function productsData() {
+  const products = await axios.get("https://fakestoreapi.com/products");
+  return products;
+}
+// ================= Products Data Fetching End here =================
